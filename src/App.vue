b0ae834a2e9962e8246f2308c6b79811834ca324
yuhaotian <3183788138@qq.com>
2024-02-03 11:23:44 +0800

    reactive 创建：对象类型的响应式数据

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <Person/>
+    <Car/>
 </template>
 
 <script lang="ts">
     //js或ts
     import Person from './components/Person.vue'
+    import Car from './components/Car.vue'
 
     export default {
         name: 'App',//组件名
         components: {
-            Person
+            Person,
+            Car
         }
     }
 </script>
---
+++ src/components/Car.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="car">
+        <!--模板无需xxx.value,自动进行 xxx.value获取数据-->
+        <h2>汽车:一辆{{ car.brand }},价值{{ car.price }}</h2>
+        <button @click="changePrice">修改价格</button>
+        <br>
+        <h2>游戏:</h2>
+        <ul>
+            <li v-for="g in games" :key='g.id'>{{ g.name }}</li>
+        </ul>
+        <button @click="changeFristGame">修改</button>
+        <br>
+        <h2>test:{{ obj.a.b.c }}</h2>
+        <button @click="changeObj">修改</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="Car">
+    import { reactive } from 'vue';
+
+    //数据
+    let car =reactive({brand:'奔驰',price:100})
+
+    let games=reactive([
+        {id:'game1',name:'g1'},
+        {id:'game2',name:'g2'},
+        {id:'game3',name:'g3'}
+    ])
+
+    let obj=reactive({
+        a:{
+            b:{
+                c:666
+            }
+        }
+    })
+
+    function changePrice(){
+        console.log(car)
+        car.price+=10
+        console.log(car)
+    }
+
+    function changeFristGame(){
+        games[0].name='g4'
+    }
+
+    function changeObj(){
+        obj.a.b.c=777
+    }
+</script>
