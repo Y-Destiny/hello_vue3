92081970d3e8ff90ab8aebde7c079a418eb10bc3
yuhaotian <3183788138@qq.com>
2024-02-03 10:42:46 +0800

    setup 语法糖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,68 +2,50 @@
     <div class="person">
         <h2>姓名：{{name}}</h2>
         <h2>年龄：{{age}}</h2>
+        <h2>地址：{{ address }}</h2>
         <button @click="showTel">查看联系方式</button>
         <button @click="changeAge">增加年龄</button>
         <button @click="changeName">修改姓名</button>
     </div>
-    <div>
-        <h1>a:{{ a }}</h1>
-        <h1>c:{{ c }}</h1>
-        <button @click="b">test</button>
-    </div>
 </template>
 
+<!--
 <script lang="ts">
     export default {
         name: 'Person',
         beforeCreate(){
             console.log("beforeCreate")
         },
-        //data和methods可与setup同时存在
-        //setup生命周期早，data可读取setup数据，setup不可读取data
-        data(){
-            return {
-                a:100,
-                c:this.name
-            }
-        },
-        methods:{
-            b(){
-                console.log('b')
-            }
-        },
-        setup(){
-            console.log("setup")
-            //setup函数中this指向undefined,vue3中弱化this
+    }
+</script>
+-->
 
-            //数据
-            let name = '张三'//name不是响应式的
-            let age = 18//age不是响应式的
-            let tel = '123456789'
+<!-- setup 语法糖 自动return-->
+<!--使用setup插件可使用name指定组件名-->
+<script lang="ts" setup name="Person">
+    console.log("setup")
+    //setup函数中this指向undefined,vue3中弱化this
 
-            //方法
-            function showTel(){
-                alert(tel)
-            }
-
-            //这样修改页面不改变
-            function changeAge(){
-                console.log(age)
-                age++
-                console.log(age)
-            }
-
-            function changeName(){
-                console.log(name)
-                name = '李四'
-                console.log(name)
-            }
-
-            //将数据和方法暴露出去，模板中才可以直接使用
-            return {name,age,showTel,changeAge,changeName}
-
-            //return function(){return 'hh'}
-            //return ()=>{return 'hh'}
-        }
+    //数据
+    let name = '张三'//name不是响应式的
+    let age = 18//age不是响应式的
+    let tel = '123456789'
+    let address='beijing'
+    //方法
+    function showTel(){
+        alert(tel)
+    }
+    
+    //这样修改页面不改变
+    function changeAge(){
+        console.log(age)
+        age++
+        console.log(age)
+    }
+    
+    function changeName(){
+        console.log(name)
+        name = '李四'
+        console.log(name)
     }
 </script>
