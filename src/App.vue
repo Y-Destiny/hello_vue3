c26d23c7507f86bbb2b75de3bec6b1581c373d83
yuhaotian <3183788138@qq.com>
2024-02-03 09:47:26 +0800

    options api实现

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
     <!--html-->
     <div class="app">
         <h1>你好</h1>
+        <Person/>
     </div>
 </template>
 
 <script lang="ts">
     //js或ts
+    import Person from './components/Person.vue'
+
     export default {
-        name: 'App'//组件名
+        name: 'App',//组件名
+        components: {
+            Person
+        }
     }
 </script>
 
---
+++ src/components/Person.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{name}}</h2>
+        <h2>年龄：{{age}}</h2>
+        <button @click="showTel">查看联系方式</button>
+        <button @click="changeAge">增加年龄</button>
+        <button @click="changeName">修改姓名</button>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Person',
+        data() {
+            return {
+                name: '张三',
+                age: 18,
+                tel: '123456789'
+            }
+        },
+        methods: {
+            changeName(){
+                this.name = '李四'
+            },
+            changeAge(){
+                this.age++
+            },
+            showTel(){
+                alert(this.tel)
+            }
+        },
+    }
+</script>
