dfd8369f3cd269afddadec589fbb1d97f0ffd089
yuhaotian <3183788138@qq.com>
2024-02-15 10:52:09 +0800

    toRef与toRefs

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -3,6 +3,7 @@
         <!--模板无需xxx.value,自动进行 xxx.value获取数据-->
         <h2>汽车:一辆{{ car.brand }},价值{{ car.price }}</h2>
         <button @click="changePrice">修改价格</button>
+        <button @click="changeCar">修改汽车</button>
         <br>
         <h2>游戏:</h2>
         <ul>
@@ -12,6 +13,9 @@
         <br>
         <h2>test:{{ obj.a.b.c }}</h2>
         <button @click="changeObj">修改</button>
+        <br>
+        <h2>sum:{{ sum }}</h2>
+        <button @click="changeSum">sum+1</button>
     </div>
 </template>
 
@@ -19,7 +23,7 @@
     import { reactive,ref } from 'vue';
 
     //数据
-    let car =ref({brand:'奔驰',price:100})
+    let car =reactive({brand:'奔驰',price:100})
 
     let games=ref([
         {id:'game1',name:'g1'},
@@ -35,10 +39,21 @@
         }
     })
 
+    //可以使用`volar`插件自动添加`.value`
+    let sum=ref(0)
+
     function changePrice(){
         console.log(car)
-        car.value.price+=10
+        car.price+=10
         console.log(car)
+    }
+
+    //`reactive`重新分配一个新对象，会**失去**响应式（可以使用`Object.assign`去整体替换）。
+    function changeCar(){
+        Object.assign(car,{brand:'宝马',price:200})
+        //car.value={brand:'宝马',price:200} 故土的刺客不更新
+        //car.value={brand:'宝马',price:200} 故土的刺客不更新
+        //car.value=reactive{brand:'宝马',price:200} 不更新
     }
 
     function changeFristGame(){
@@ -47,5 +62,9 @@
 
     function changeObj(){
         obj.a.b.c=777
+    }
+
+    function changeSum(){
+        sum.value++
     }
 </script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="person">
         <!--模板无需xxx.value,自动进行 xxx.value获取数据-->
-        <h2>姓名：{{name}}</h2>
-        <h2>年龄：{{age}}</h2>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
         <h2>地址：{{ address }}</h2>
-        <button @click="showTel">查看联系方式</button>
         <button @click="changeAge">增加年龄</button>
         <button @click="changeName">修改姓名</button>
+        <button @click="changeAddress">修改地址</button>
     </div>
 </template>
 
@@ -24,33 +24,34 @@
 <!-- setup 语法糖 自动return-->
 <!--使用setup插件可使用name指定组件名-->
 <script lang="ts" setup name="Person">
-    console.log("setup")
-    //setup函数中this指向undefined,vue3中弱化this
+    import { reactive,toRefs,toRef } from 'vue';
 
-    //ref 创建：基本类型的响应式数据
-    import {ref} from 'vue'
+    let person=reactive({
+        name:'张三',
+        age:18,
+        address:'北京',
+        tel:'1234567890'
+    })
 
-    //数据
-    //使用ref()包裹数据变为响应式
-    let name = ref('张三')
-    let age = ref(18)
-    let tel = '123456789'
-    let address='beijing'
-    //方法
-    function showTel(){
-        alert(tel)
-    }
-    
-    //这样修改页面不改变
+    //作用：将一个响应式对象中的每一个属性，转换为`ref`对象。
+    //备注：`toRefs`与`toRef`功能一致，但`toRefs`可以批量转换。
+
+     // 通过toRefs将person对象中的n个属性批量取出，且依然保持响应式的能力
+    let {name,age}=toRefs(person)
+
+    // 通过toRef将person对象中的gender属性取出，且依然保持响应式的能力
+    let address = toRef(person,'address')
+
     function changeAge(){
-        console.log(age)
-        age.value++
-        console.log(age)
+        age.value+=1
     }
-    
+
     function changeName(){
-        console.log(name)
-        name.value = '李四'
-        console.log(name)
+        name.value='李四'
     }
+
+    function changeAddress(){
+        address.value='上海'
+    }
+
 </script>
