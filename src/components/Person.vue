a06e8ef989d0739027c9b89d6c8f0e56b9aa310c
yuhaotian <3183788138@qq.com>
2024-02-15 11:26:27 +0800

    计算属性

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,57 +1,47 @@
 <template>
     <div class="person">
-        <!--模板无需xxx.value,自动进行 xxx.value获取数据-->
-        <h2>姓名：{{ name }}</h2>
-        <h2>年龄：{{ age }}</h2>
-        <h2>地址：{{ address }}</h2>
-        <button @click="changeAge">增加年龄</button>
-        <button @click="changeName">修改姓名</button>
-        <button @click="changeAddress">修改地址</button>
+        <h2>person</h2>
+        姓：<input type="text" v-model="firstName"><br>
+        名：<input type="text" v-model="lastName"><br>
+        全名：<span>{{ fullName }}</span><br>
+        <button @click="changeName">修改名字</button>
     </div>
 </template>
-
-<!--
-<script lang="ts">
-    export default {
-        name: 'Person',
-        beforeCreate(){
-            console.log("beforeCreate")
-        },
-    }
-</script>
--->
 
 <!-- setup 语法糖 自动return-->
 <!--使用setup插件可使用name指定组件名-->
 <script lang="ts" setup name="Person">
-    import { reactive,toRefs,toRef } from 'vue';
+    import { reactive,ref,computed } from 'vue';
 
-    let person=reactive({
-        name:'张三',
-        age:18,
-        address:'北京',
-        tel:'1234567890'
+    let firstName=ref('张')
+    let lastName=ref('三')
+
+    //计算属性
+
+    /*
+    //只读
+    let fullName=computed(()=>{
+        return firstName.value+'-'+lastName.value
+    })
+    */
+
+    //读写
+    let fullName=computed({
+        get(){
+            return firstName.value+'-'+lastName.value
+        },
+        set(val){
+            console.log(val)
+            let names=val.split('-')
+            firstName.value=names[0]
+            lastName.value=names[1]
+        }
     })
 
-    //作用：将一个响应式对象中的每一个属性，转换为`ref`对象。
-    //备注：`toRefs`与`toRef`功能一致，但`toRefs`可以批量转换。
-
-     // 通过toRefs将person对象中的n个属性批量取出，且依然保持响应式的能力
-    let {name,age}=toRefs(person)
-
-    // 通过toRef将person对象中的gender属性取出，且依然保持响应式的能力
-    let address = toRef(person,'address')
-
-    function changeAge(){
-        age.value+=1
-    }
-
     function changeName(){
-        name.value='李四'
+        console.log(fullName.value)
+        fullName.value='李-四'
     }
 
-    function changeAddress(){
-        address.value='上海'
-    }
 
 </script>
