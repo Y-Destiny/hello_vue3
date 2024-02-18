c3c9d4b97e1f232ba89193067a4c2598c49c7532
yuhaotian <3183788138@qq.com>
2024-02-18 17:55:06 +0800

    组合式store

--- src/store/loveTalk.ts
+++ src/store/loveTalk.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 import { nanoid } from "nanoid";    
-
+/*选项式
 export const useTalkStore = defineStore('talk', {
     actions:{
         async getATalk(){
@@ -16,3 +16,24 @@ export const useTalkStore = defineStore('talk', {
         return {'talkList': JSON.parse(localStorage.getItem('talkList')||'[]')}
     }
 })
+*/
+//组合式
+import { ref } from "vue";
+export const useTalkStore = defineStore('talk', ()=>{
+    
+    const talkList = ref(JSON.parse(localStorage.getItem('talkList') as string)||'[]')
+
+    async function getATalk(){
+        let { data: { content } } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+
+        let obj = { id: nanoid(), content };
+
+        talkList.value.unshift(obj);
+
+    }
+
+    
+    return { talkList, getATalk }
+})
+
+
