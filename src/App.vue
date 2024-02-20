38f13c7eda4b9cfdf3d71f0d5cfab91b936502d6
yuhaotian <3183788138@qq.com>
2024-02-20 10:10:46 +0800

    readonly shallowReadonly

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,36 @@
 <template>
 	<div class="container-fluid wraper">
-		<h2>求和：{{ sum }}</h2>
-		<h2>姓名：{{ person.name }}</h2>
-		<h2>年龄：{{ person.age }}</h2>
-		<h2>汽车：{{ car.brand }}</h2>
-		<h2>价格：{{ car.price }}</h2>
-		<h2>颜色：{{ car.Options.color }}</h2>
+		<h2>sum：{{ sum }}</h2>
+		<h2>sum2：{{ sum2 }}</h2>
 		<button @click="changeSum">点我sum+1</button>
-		<button @click="changeAge">点我age+1</button>
-		<button @click="changeName">点我改名</button>
-		<button @click="changePerson">点我重置</button>
-		<button @click="changeBrans">修改brand</button>
-		<button @click="changePrice">修改price</button>
-		<button @click="changeColor">修改颜色</button>
-		<button @click="changeCar">修改汽车</button>
+		<button @click="changeSum2">点我sum2+1</button>
+
+		<h2>car.name：{{ car.name }}</h2>
+		<h2>car.options.color：{{ car.options.color }}</h2>
+		<button @click="car.name='宝马'">点我改变car.name</button>
+		<button @click="car.options.color='blue'">点我改变car.options.color</button>
+		<h2>{{ car2 }}</h2>
+		<button @click="car2.name='奥迪'">点我改变car2.name</button>
+		<button @click="car2.options.color='yellow'">点我改变car2.options.color</button>
 	</div>
 </template>
 
 <script setup lang="ts" name="App">
-import { ref, reactive, shallowReactive, shallowRef } from 'vue'
+import { ref,reactive, readonly, shallowReadonly } from 'vue'
 
-let sum= shallowRef(0)
-let person = shallowRef({
-	name: 'John111',
-	age: 30
-})
-let car= shallowReactive({
-	brand: 'Benz',
-	price: 1000000,
-	Options: {
-		color: 'red'
-	}
-})
+let sum= ref(0)
+let sum2=readonly(sum)
+
+let car = reactive({name: '奔驰' ,options:{color:'red'}})
+
+let car2 = shallowReadonly(car)
+
 function changeSum() {
 	sum.value++
 }
 
-function changeAge() {
-	person.value.age++
-}
-
-function changeName() {
-	person.value.name = 'Tom'
-}
-
-function changePerson() {
-	person.value = {
-		name: 'John',
-		age: 25
-	}
-}
-
-function changeBrans() {
-	car.brand = 'Audsasi'
-}
-
-function changePrice() {
-	car.price +=1
-}
-
-function changeColor() {
-	car.Options.color = 'blue'
-}
-
-function changeCar() {
-	Object.assign(car , {
-		brand: 'BMW',
-		price: 3000000,
-		Options: {
-			color: 'black'
-		}
-	})
+function changeSum2() {
+	sum2.value++
 }
 
 
